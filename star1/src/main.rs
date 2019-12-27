b66e521aad1bc65fd997cc1f84fe62c4ef6dfd8c
Steve Franchak <stevefranchak@gmail.com>
2019-12-27 17:57:29 -0500

    Terminates stdin iterator on first empty line instead of panicking

--- star1/src/main.rs
+++ star1/src/main.rs
@@ -8,9 +8,10 @@ fn convert_to_fuel(mass: u32) -> u32 {
 fn main() -> io::Result<()> {
     let stdin = io::stdin();
     let fuel_iter = stdin.lock().lines()
+        .take_while(|l| l.as_ref().expect("could not read line").len() > 0)
         .map(|l| convert_to_fuel(
             l.unwrap().parse::<u32>().expect("could not parse input line as u32")
         ));
-    println!("\nSum: {:?}", fuel_iter.sum::<u32>());
+    println!("Sum: {:?}", fuel_iter.sum::<u32>());
     Ok(())
 }
