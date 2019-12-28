b9d4c3feabe993f79b8eb6a999190cec61b649f6
Steve Franchak <stevefranchak@gmail.com>
2019-12-27 20:44:11 -0500

    Received second star

--- star2/src/main.rs
+++ star2/src/main.rs
@@ -1,8 +1,12 @@
 use std::io;
 use std::io::prelude::*;
 
-fn convert_to_fuel(mass: u32) -> u32 {
-    mass / 3 - 2
+fn convert_to_fuel(mass: i32) -> i32 {
+    let fuel = mass / 3 - 2;
+    if fuel <= 0 {
+        return 0;
+    }
+    fuel + convert_to_fuel(fuel)
 }
 
 fn main() -> io::Result<()> {
@@ -10,8 +14,8 @@ fn main() -> io::Result<()> {
     let fuel_iter = stdin.lock().lines()
         .take_while(|l| l.as_ref().expect("could not read line").len() > 0)
         .map(|l| convert_to_fuel(
-            l.unwrap().parse::<u32>().expect("could not parse input line as u32")
+            l.unwrap().parse::<i32>().expect("could not parse input line as i32")
         ));
-    println!("Sum: {}", fuel_iter.sum::<u32>());
+    println!("Sum: {}", fuel_iter.sum::<i32>());
     Ok(())
 }
